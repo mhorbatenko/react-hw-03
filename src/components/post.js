0f1d2432099e2cdaf7dd4bdfc550afa6e8c57f10
mhorbatenko <fenukraine@gmail.com>
2023-08-03 14:06:08 +0300

    ref

--- src/components/post.js
+++ src/components/post.js
@@ -1,25 +1,15 @@
 
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
+
 import Typography from '@mui/material/Typography';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import {SkipNextIcon} from '@mui/icons-material/SkipNext';
 import Grid from '@mui/material/Grid';
 import {Paper} from "@mui/material";
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { Button } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 
@@ -63,12 +53,6 @@ export  default function Post({id, title, body, removePost}) {
             <DialogTitle id="alert-dialog-title">
               Please confirm removing post with id "{id}"?
             </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Let Google help apps determine location. This means sending anonymous
-                location data to Google, even when no apps are running.
-              </DialogContentText>
-            </DialogContent>
             <DialogActions>
               <Button onClick={handleClose}>Disagree</Button>
               <Button onClick={() => handleRemoveConfirm(id)} autoFocus>
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,30 +1,15 @@
 "use client"
-import { Card, CardContent, Grid, Item } from "@mui/material"
-import {Typography} from "@mui/material"
-import Box from '@mui/system/Box';
+import { Grid } from "@mui/material"
 import { useState, useEffect } from "react"
 import { Button } from "@mui/material";
 import Post from '@/components/post'
 import AddIcon from '@mui/icons-material/Add';
-import CloseIcon from '@mui/icons-material/Close';
-
-
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-
-
-
 import uuidV4 from "@/app/utils/uuid";
 
 
 export default function Posts() {
 
     const [posts, setPosts] = useState([])
-    const [agreement, SetAgreement] = useState(false)
-
 
     const getApiData = async () => {
         const response = await fetch(
@@ -38,8 +23,7 @@ export default function Posts() {
     }, []);
 
     function addPostHandler() {
-        const updatedPosts = [...posts, {id: uuidV4(), title: 'new', content: 'foo-bar'}]
-        updatedPosts.reverse()
+        const updatedPosts = [{id: uuidV4(), title: 'Write post title', body: 'Write post body'}, ...posts]
         setPosts(updatedPosts)
     }
 
@@ -49,15 +33,6 @@ export default function Posts() {
         });
         setPosts(updatedPosts)
     }
-
-    const handleClickOpen = () => {
-        SetAgreement(true);
-      };
-    
-      const handleClose = () => {
-        SetAgreement(false);
-      };
-
     return (
         <Grid container gap={2}>
         <Grid item xs={12}>
